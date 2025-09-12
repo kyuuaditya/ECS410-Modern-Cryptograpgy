e99397829edec4a5f342bc994c4f16475eff2534
Aditya <thisisaditya.me@gmail.com>
2025-09-12 09:40:28 +0530

    adder substitution_cypher

--- crypto.h
+++ crypto.h
@@ -4,10 +4,11 @@
 
 class Crypto {
 public:
+    std::string alphabets_lower = "abcdefghijklmnopqrstuvwxyz";
+    std::string alphabets_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     std::string shift_cypher(const std::string& message, int key) {
         std::string result;
-        std::string alphabets_lower = "abcdefghijklmnopqrstuvwxyz";
-        std::string alphabets_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
         for (char character : message) {
             if (islower(character)) {
@@ -33,5 +34,30 @@ public:
 
     std::string shift_cypher_decode(const std::string& message, int key) {
         return shift_cypher(message, -key);
+    }
+
+    std::string substitution_cypher(const std::string& message, const std::string& key) {
+        std::string result;
+        
+        for (char character : message) {
+            if (islower(character)) {
+                size_t index = alphabets_lower.find(character);
+                if (index != std::string::npos && key.size() == alphabets_lower.size()) {
+                    result += key[index];
+                } else {
+                    result += character;
+                }
+            } else if (isupper(character)) {
+                size_t index = alphabets_upper.find(character);
+                if (index != std::string::npos && key.size() == alphabets_upper.size()) {
+                    result += key[index];
+                } else {
+                    result += character;
+                }
+            } else {
+                result += character;
+            }
+        }
+        return result;
     }
 };
--- test.cpp
+++ test.cpp
@@ -11,18 +11,25 @@ int main() {
 
     std::cout << "Enter message: ";
     std::getline(std::cin, message);
-    std::cout << "Enter key: ";
-    std::cin >> key;
+    // std::cout << "Enter key: ";
+    // std::cin >> key;
 
     // ------------------------------------------------------------------------------------------------
-    // test for applying shift cypher and then decoding it back
-    std::string ciphered_message = crypto.shift_cypher(message, key);
-    std::cout << "Ciphered message: " << ciphered_message << std::endl;
 
-    std::string deciphered_message = crypto.shift_cypher_decode(ciphered_message, key);
-    std::cout << "Deciphered message: " << deciphered_message << std::endl;
+    // // test for applying shift cypher and then decoding it back
+    // std::string ciphered_message = crypto.shift_cypher(message, key);
+    // std::cout << "Ciphered message: " << ciphered_message << std::endl;
+
+    // std::string deciphered_message = crypto.shift_cypher_decode(ciphered_message, key);
+    // std::cout << "Deciphered message: " << deciphered_message << std::endl;
+
     // ------------------------------------------------------------------------------------------------
 
+    // test for substitution cypher
+    std::string substitution_key = "QWERTYUIOPASDFGHJKLZXCVBNM"; // simple reverse alphabet substitution
+    std::string substituted_message = crypto.substitution_cypher(message, substitution_key);
+    std::cout << "Substituted message: " << substituted_message << std::endl;
 
+    // ------------------------------------------------------------------------------------------------
     return 0;
 }
